8e0abf2849a3a4e420895600974f41e1ba3cd950
nhanluongoe <nhanlt18@gmail.com>
2022-07-14 16:27:16 +0700

    chore: change name of application

--- src/main/java/com/example/demo/QuotieApplication.java
+++ src/main/java/com/example/demo/QuotieApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class QuotieApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(QuotieApplication.class, args);
 	}
 
 }
