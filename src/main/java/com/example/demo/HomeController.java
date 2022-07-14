361cbbb7a3e47e493b0381fb7506f00bc3359d61
nhanluongoe <nhanlt18@gmail.com>
2022-07-14 16:27:41 +0700

    chore: add example home page

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+  @GetMapping("/")
+  public String home(@RequestParam(name="name", required = false, defaultValue = "World") String name, Model model) {
+    model.addAttribute("name", name);
+    return "home";
+  }
+}
