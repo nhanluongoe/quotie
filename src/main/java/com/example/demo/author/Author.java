b9253f5cf30b2bfb276ab880bdd21b1c6cf04f35
nhanluongoe <nhanlt18@gmail.com>
2022-07-14 20:06:21 +0700

    feat(author): cascade on author

--- src/main/java/com/example/demo/author/Author.java
+++ src/main/java/com/example/demo/author/Author.java
@@ -2,6 +2,7 @@ package com.example.demo.author;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -23,7 +24,7 @@ public class Author {
   @Column(name = "name")
   private String name;
 
-  @OneToMany(mappedBy = "author")
+  @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
   private List<Quote> quotes;
 
   public Long getId() {
