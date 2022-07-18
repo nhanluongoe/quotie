e13f39a307dd3c591f34ffa37ce40502a847b5d3
nhanluongoe <nhanlt18@gmail.com>
2022-07-18 17:34:15 +0700

    feat(quote): add quote controller

---
+++ src/main/java/com/example/demo/quote/QuoteController.java
@@ -0,0 +1,28 @@
+package com.example.demo.quote;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/quote")
+public class QuoteController {
+
+  private QuoteService quoteService;
+
+  @Autowired
+  public QuoteController(QuoteService quoteService) {
+    this.quoteService = quoteService;
+  }
+
+  @GetMapping
+  public String quoteList(Model model) {
+    List<Quote> quotes = quoteService.getAllQuotes();
+    model.addAttribute("quotes", quotes);
+    return "quote-list";
+  }
+}
