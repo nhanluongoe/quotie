05fc68c1355b7b387083c938a355fe60065d288e
nhanluongoe <nhanlt18@gmail.com>
2022-07-18 17:37:05 +0700

    refactor(author): use "/author" as prefix for all author route

--- src/main/java/com/example/demo/author/AuthorController.java
+++ src/main/java/com/example/demo/author/AuthorController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(path = "/author")
 public class AuthorController {
 
   private AuthorService authorService;
@@ -17,7 +19,7 @@ public class AuthorController {
     this.authorService = authorService;
   }
 
-  @GetMapping("/author")
+  @GetMapping
   public String authorList(Model model) {
     List<Author> authors = authorService.getAllAuthors();
     model.addAttribute("authors", authors);
