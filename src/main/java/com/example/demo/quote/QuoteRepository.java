c5a8cdc4f73cf3f1823ba0b1e1f483e021364e29
nhanluongoe <nhanlt18@gmail.com>
2022-07-21 16:23:16 +0700

    feat(quote): add custom query for querying list of quotes by tag id

--- src/main/java/com/example/demo/quote/QuoteRepository.java
+++ src/main/java/com/example/demo/quote/QuoteRepository.java
@@ -1,6 +1,14 @@
 package com.example.demo.quote;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
+
+  @Query(value = "SELECT * FROM quote q INNER JOIN quote_tag qt on q.id = qt.quote_id WHERE qt.tag_id = ?1", nativeQuery = true)
+  public List<Quote> findAllByTag(Long tag_id);
 }
