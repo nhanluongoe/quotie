8af4c39e1b23225ee97a32df0abb1d638d077728
nhanluongoe <nhanlt18@gmail.com>
2022-07-22 09:05:26 +0700

    feat(author): add custom query to get list of first authors by name letter

--- src/main/java/com/example/demo/author/AuthorRepository.java
+++ src/main/java/com/example/demo/author/AuthorRepository.java
@@ -1,13 +1,16 @@
 package com.example.demo.author;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthorRepository extends JpaRepository<Author, Long>{
-  List<Author> findAll();
-  Optional<Author> findById(Long id);
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+  @Query(value = "SELECT * FROM author a WHERE LOWER(a.name) LIKE ?1%", nativeQuery = true)
+  public List<Author> findAuthorsByFirstLetterName(Character letter);
+
+  @Query(value = "SELECT * FROM author a WHERE LOWER(a.name) LIKE ?1% LIMIT ?2", nativeQuery = true)
+  public List<Author> findFirstAuthorsByFirstLetterName(Character letter, Integer limit);
 }
