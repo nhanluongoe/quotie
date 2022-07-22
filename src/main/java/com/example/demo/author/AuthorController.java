18a1b543b3d14e6b72b49ee1cf43093728978260
nhanluongoe <nhanlt18@gmail.com>
2022-07-22 14:35:17 +0700

    feat(author): add controller for author detail

--- src/main/java/com/example/demo/author/AuthorController.java
+++ src/main/java/com/example/demo/author/AuthorController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.util.Util;
@@ -38,5 +39,11 @@ public class AuthorController {
     model.addAttribute("authorsByFirstLetterName", authorsByFirstLetterNameList);
 
     return "author/author";
+  }
+
+  @GetMapping(value = "{authorId}")
+  public String authorDetail(@PathVariable("authorId") Long authorId, Model model) {
+    model.addAttribute("author", authorService.getAuthorById(authorId));
+    return "author/author-detail";
   }
 }
