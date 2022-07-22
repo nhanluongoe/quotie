cea2730c40a6b1391fce494e7fe1375d1c0d52a1
nhanluongoe <nhanlt18@gmail.com>
2022-07-22 09:04:22 +0700

    feat(author): add list of first top authors

--- src/main/java/com/example/demo/author/AuthorController.java
+++ src/main/java/com/example/demo/author/AuthorController.java
@@ -1,5 +1,6 @@
 package com.example.demo.author;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.util.Util;
 
 @Controller
 @RequestMapping(path = "/author")
@@ -23,6 +26,17 @@ public class AuthorController {
   public String authorList(Model model) {
     List<Author> authors = authorService.getAllAuthors();
     model.addAttribute("authors", authors);
-    return "author/author-list";
+
+    List<Character> letters = Util.convertStringToCharList("abcdefghijklmnopqrstuvwxyz");
+    model.addAttribute("letters", letters);
+
+    List<List<Author>> authorsByFirstLetterNameList = new ArrayList<>();
+    for (Character letter : letters) {
+      List<Author> authorsByFirstLetterName = authorService.getFirstAuthorsByFirstNameLetter(letter, 10);
+      authorsByFirstLetterNameList.add(authorsByFirstLetterName);
+    }
+    model.addAttribute("authorsByFirstLetterName", authorsByFirstLetterNameList);
+
+    return "author/author";
   }
 }
