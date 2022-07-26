6f92e9c768e4d2b116c03ed8290be46d83727571
nhanluongoe <nhanlt18@gmail.com>
2022-07-26 22:24:44 +0700

    feat(home): add search handler

--- src/main/java/com/example/demo/home/HomeController.java
+++ src/main/java/com/example/demo/home/HomeController.java
@@ -1,10 +1,19 @@
 package com.example.demo.home;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.quote.Quote;
 import com.example.demo.quote.QuoteService;
 
 @Controller
@@ -23,5 +32,21 @@ public class HomeController {
     model.addAttribute("popular_quotes", quoteService.getPopularQuotes());
     model.addAttribute("random_quote", quoteService.getRandomQuote());
     return "home/home";
+  }
+
+  @GetMapping(value = "/search")
+  public String search(@RequestParam(defaultValue = "") String query, @RequestParam(defaultValue = "0") Integer page,
+      Model model) {
+    Pageable pagination = PageRequest.of(page, 10);
+    Page<Quote> quotesPage = quoteService.getQuotesByQuery(query, pagination);
+    model.addAttribute("quotesPage", quotesPage);
+
+    int totalPages = quotesPage.getTotalPages();
+    if (totalPages > 0) {
+      List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+      model.addAttribute("pageNumbers", pageNumbers);
+    }
+
+    return "search/search";
   }
 }
