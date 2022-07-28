723de714076e26d58fb97cf9f8c4c89dd88e2691
nhanluongoe <nhanlt18@gmail.com>
2022-07-28 17:11:33 +0700

    feat(security): add web security configuration

---
+++ src/main/java/com/example/demo/security/WebSecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.example.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.example.demo.user.UserDetailServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private UserDetailServiceImpl userDetailServiceImpl;
+
+  @Autowired
+  private PasswordEncoder bcryptPasswordEncoder;
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailServiceImpl).passwordEncoder(bcryptPasswordEncoder);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/admin").hasRole("ADMIN")
+        .antMatchers("/profile").authenticated()
+        .and()
+        .formLogin().loginPage("/login").failureForwardUrl("/login?error=true")
+        .defaultSuccessUrl("/")
+        .usernameParameter("username")
+        .passwordParameter("password")
+        .and().logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .logoutSuccessUrl("/login").and().exceptionHandling();
+  }
+}
