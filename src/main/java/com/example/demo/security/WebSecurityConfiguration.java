357f627a84ba3508b48b3cdac5a85bcea64b97fd
nhanluongoe <nhanlt18@gmail.com>
2022-07-28 21:10:01 +0700

    fix(security): secure admin path

--- src/main/java/com/example/demo/security/WebSecurityConfiguration.java
+++ src/main/java/com/example/demo/security/WebSecurityConfiguration.java
@@ -29,7 +29,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-        .antMatchers("/admin").hasRole("ADMIN")
+        .antMatchers("/admin/**").hasRole("ADMIN")
         .antMatchers("/profile").authenticated()
         .and()
         .formLogin().loginPage("/login").failureForwardUrl("/login?error=true")
