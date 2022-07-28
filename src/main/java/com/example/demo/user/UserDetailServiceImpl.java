462c776e4d951d6d72823653c432d59bfeebb532
nhanluongoe <nhanlt18@gmail.com>
2022-07-28 17:14:16 +0700

    feat(security): add user details implmentation

---
+++ src/main/java/com/example/demo/user/UserDetailServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.user;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.role.Role;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService{
+
+  @Autowired
+  private UserService userService;
+
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+    User user = userService.getUserByUsername(username);
+    List<GrantedAuthority> authorities = getUserAuthority(user.getRoles());
+    return buildUserForAuthentication(user, authorities);
+  }
+
+  private List<GrantedAuthority> getUserAuthority(Set<Role> userRoles) {
+    Set<GrantedAuthority> roles = new HashSet<GrantedAuthority>();
+    for (Role role : userRoles) {
+        roles.add(new SimpleGrantedAuthority(role.getRole()));
+    }
+    List<GrantedAuthority> grantedAuthorities = new ArrayList<>(roles);
+    return grantedAuthorities;
+}
+
+private UserDetails buildUserForAuthentication(User user, List<GrantedAuthority> authorities) {
+    return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+            user.getActive(), true, true, true, authorities);
+}
+}
