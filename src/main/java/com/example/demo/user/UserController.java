a46b0f19f83ffa503de1d47268ef9d0f928058f0
nhanluongoe <nhanlt18@gmail.com>
2022-07-31 11:52:57 +0700

    feat(user): handle user details information

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -13,10 +13,19 @@ public class UserController {
   @Autowired
   private UserService userService;
 
+  @Autowired
+  private UserDetailsService userDetailsService;
+
   @GetMapping(value = "/profile")
   public String profilePage(Principal principal, Model model) {
     User currentUser = userService.getUserByUsername(principal.getName());
     model.addAttribute("currentUser", currentUser);
+
+    UserDetails userDetails = userDetailsService.getUserDetailsByUserId(currentUser.getId());
+    if (userDetails != null) {
+      model.addAttribute("userDetails", userDetails);
+    }
+
     return "profile/index";
   }
 }
