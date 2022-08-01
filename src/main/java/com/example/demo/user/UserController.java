f7cc641d95c49ee2f3b7d4e0faa23bef7aba7286
nhanluongoe <nhanlt18@gmail.com>
2022-08-01 11:28:46 +0700

    refactor(user): use base path for user controller

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -9,11 +9,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.quote.Quote;
 import com.example.demo.quote.QuoteService;
 
 @Controller
+@RequestMapping(value = "/profile")
 public class UserController {
 
   @Autowired
@@ -25,7 +27,7 @@ public class UserController {
   @Autowired
   private QuoteService quoteService;
 
-  @GetMapping(value = "/profile")
+  @GetMapping
   public String profilePage(Principal principal, Model model) {
     User user = userService.getUserByUsername(principal.getName());
     model.addAttribute("user", user);
