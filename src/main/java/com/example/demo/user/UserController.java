f4f43cac1f01eb4c88e7bdb4d1d6258a93f4d221
nhanluongoe <nhanlt18@gmail.com>
2022-08-01 11:53:21 +0700

    feat(user): handle see more favorite quotes

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -1,6 +1,9 @@
 package com.example.demo.user;
 
 import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.quote.Quote;
 import com.example.demo.quote.QuoteService;
@@ -45,5 +49,24 @@ public class UserController {
     }
 
     return "profile/index";
+  }
+
+  @GetMapping(value = "/quote")
+  public String profileQuote(@RequestParam(defaultValue = "0") Integer page, Principal principal, Model model) {
+    User user = userService.getUserByUsername(principal.getName());
+    model.addAttribute("user", user);
+    UserDetails userDetails = userDetailsService.getUserDetailsByUserId(user.getId());
+
+    Pageable pagination = PageRequest.of(page, 12);
+    Page<Quote> quotesPage = quoteService.getQuotesByUserDetails(userDetails, pagination);
+    model.addAttribute("quotesPage", quotesPage);
+
+    int totalPages = quotesPage.getTotalPages();
+    if (totalPages > 0) {
+      List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+      model.addAttribute("pageNumbers", pageNumbers);
+    }
+
+    return "profile/quote";
   }
 }
