46f252916c563919914a874ac34d89fbd417b265
nhanluongoe <nhanlt18@gmail.com>
2022-08-02 23:51:05 +0700

    feat(comment): add comment model

---
+++ src/main/java/com/example/demo/comment/Comment.java
@@ -0,0 +1,27 @@
+package com.example.demo.comment;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "content", columnDefinition = "TEXT")
+  @NotEmpty(message = "Content cannot be empty!")
+  private String content;
+}
