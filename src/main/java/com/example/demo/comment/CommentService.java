442e214db7bf35a9b44b46653e5ac6ea2c8b2830
nhanluongoe <nhanlt18@gmail.com>
2022-08-03 21:25:06 +0700

    feat(comment): add comment service

---
+++ src/main/java/com/example/demo/comment/CommentService.java
@@ -0,0 +1,20 @@
+package com.example.demo.comment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.quote.Quote;
+
+@Service
+public class CommentService {
+
+  @Autowired
+  private CommentRepository commentRepository;
+
+  public Page<Comment> getCommentsByQuote(Quote quote, Pageable pageable) {
+    return commentRepository.findByQuote(quote, pageable);
+  }
+
+}
